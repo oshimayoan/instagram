cb801a26e16230f3ef54262b7a45bfe7d16aca9f
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-08-31 16:31:16 +0700

    show icon only for tab

--- src/routes/Main.tsx
+++ src/routes/Main.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { IconButton } from 'exoflex';
 
-import {Feed, Profile} from '../screens';
+import { Feed, Profile } from '../screens';
 
 const Tab = createBottomTabNavigator();
 
 export default () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        backBehavior="history"
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName = '';
+
+            if (route.name === 'Feed') {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'account' : 'account-outline';
+            }
+
+            return <IconButton icon={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{ showLabel: false }}
+      >
         <Tab.Screen name="Feed" component={Feed} />
         <Tab.Screen name="Profile" component={Profile} />
       </Tab.Navigator>
