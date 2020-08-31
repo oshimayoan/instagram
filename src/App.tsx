76ef6a41372b9a38ed55ad105202bf9cafd294a1
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2020-08-31 16:16:15 +0700

    install exoflex

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { Provider as ThemeProvider } from 'exoflex';
 
 import Router from './routes/Main';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider>
       <StatusBar style={'auto'} />
       <Router />
-    </>
+    </ThemeProvider>
   );
 }
